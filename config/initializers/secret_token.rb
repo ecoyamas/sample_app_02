50e03088ca98f77dba3b966e6e6fd66afd3a08fd
ecoyamas <hayato53pu@gmail.com>
2018-05-07 22:42:15 +0900

    sign_in_error

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp02::Application.config.secret_key_base = 'a705cc6dd78c2ca4ad37ae5a793fa9ae627379e8d3b87015fb38f8166ecd6ab1862db7a830f35b49517400bbf8623a6d4f74000c6e9c56b5ee51defd042b644e'
